06052a941ab2f80913ab5664b64e24b2ee59d5be
Shimada Keiki <shimada.cake@gmail.com>
2010-12-27 22:04:29 +0900

    ゲーム版を表示（コンパイルエラー）

---
+++ Disp.scala
@@ -0,0 +1,26 @@
+package com.sukarabu.sgsoccer
+
+object Main{
+    def main(args:Array[String]) {
+        clearScreen
+        showBoard
+    }
+
+    def clearScreen = {
+        print("[2J")
+    }
+
+    def showBoard = {
+        println("==== SHO-GUN SOCCER ====")
+        println("          1         2         3")
+        println(" 123456789012345678901234567890")
+        println("+------------------------------+")
+        for (i <- 1 to 12) {
+            print("|..............................|")
+            println(String.format("% 2d", i))
+        }
+        println("+------------------------------+")
+    }
+}
+
+// vim: set ts=4 sw=4 et:
